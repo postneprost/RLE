d8ccfcc7e6c4c779be4dc49849c2a89298a007e5
postneprost <postneprost@yandex.ru>
2020-03-21 17:34:25 +0500

    Update rle.js
    test

--- rle.js
+++ rle.js
@@ -1,9 +1,14 @@
 function rle(str) {
-    let result;
+    let result = '';
+    let count = 1;
     for (let i = 0; i < str.length; i++) {
         if (str[i] == str[i+1]) count++;
-        else count = 1;
+        else {
+            result += count + str[i];
+            count = 1;
+        }
     }
+    return result;
 }
 
 
